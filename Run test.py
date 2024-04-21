ada195ba72c3c58d0a0c229467a36909bb134772
jackcramer <156267708+jackcramer@users.noreply.github.com>
2024-04-21 18:41:13 -0400

    Add files via upload

---
+++ Run test.py
@@ -0,0 +1,58 @@
+import random
+
+# List of words (countries or capitals)
+WORDS = ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cabo Verde", "Cambodia", "Cameroon", "Canada", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of the Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "East Timor", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Eswatini", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Ivory Coast", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kosovo", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Monaco", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "North Macedonia", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican City", "Venezuela", "Vietnam", "Yemen", "Zambia", "Zimbabwe]
+
+def choose_word():
+    """Randomly chooses a word from the list."""
+    return random.choice(WORDS)
+
+def display_word(word, guessed_letters):
+    """Displays the word with blanks for letters not guessed."""
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
+
+def hangman():
+    """Main function to run the Hangman game."""
+    word = choose_word().upper()
+    guessed_letters = []
+    attempts = 6
+
+    print("Welcome to Geography Hangman!")
+    print("You need to guess a country or capital. Let's start!\n")
+
+    while True:
+        print("Attempts left:", attempts)
+        print("Current word:", display_word(word, guessed_letters))
+
+        guess = input("Enter a letter or guess the word: ").upper()
+
+        if guess == word:
+            print("Congratulations! You've guessed the word correctly:", word)
+            break
+        elif guess in guessed_letters:
+            print("You've already guessed that letter. Try again.")
+            continue
+        elif len(guess) != 1 or not guess.isalpha():
+            print("Please enter a single letter or the full word.")
+            continue
+        elif guess in word:
+            print("Correct guess!")
+            guessed_letters.append(guess)
+            if set(guessed_letters) == set(word):
+                print("Congratulations! You've guessed the word correctly:", word)
+                break
+        else:
+            print("Incorrect guess.")
+            guessed_letters.append(guess)
+            attempts -= 1
+            if attempts == 0:
+                print("Sorry, you've run out of attempts. The word was:", word)
+                break
+
+hangman()
